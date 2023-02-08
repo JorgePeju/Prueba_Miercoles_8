c7d9cbb4421fd0f61a1e5f1b7e683a7d6a2073ee
JorgePeju <pejuco@gmail.com>
2023-02-08 09:38:41 +0100

    1

---
+++ PruebaM8.js
@@ -0,0 +1,42 @@
+//Solicita al usuario 3 palabras, 
+//1. de la primera debes mostrar su segunda letra, 
+function palabra1() {
+
+var palabra1 = prompt("Introduzca la primera palabra")
+var resultado1 = ""
+
+for (let i = 0; i < palabra1.length; i++) {
+    
+    if (
+    console.log(resultado1)
+}
+
+}
+//2. de la segunda su tamaño y 
+
+function palabra2() {
+    
+
+var palabra2 = prompt("Introduzca la primera palabra")
+var resultado = ""
+
+for (let i = 0; i < palabra2.length; i++) {
+ 
+    if (resultado2 =  i + 1) {
+    }
+    
+}
+
+alert("Tu número es " + resultado2)
+
+}
+
+
+//3.de la tercera las vocales que se encuentren en posiciones impares.
+
+function palabra3() {
+
+    var palabra3 = prompt("Introduzca la primera palabra")
+
+
+}
